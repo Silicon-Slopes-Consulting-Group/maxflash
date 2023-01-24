1ddc508c41631311fa411265f56a7935e1d0cf90
Steven Hooley <steven@hooley.me>
2023-01-24 02:30:08 -0700

    update

--- firebase.js
+++ firebase.js
@@ -11,12 +11,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   //Update these values as per the firebase.js file you receive from Firebase
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyD4k2O3wM477B8aAu5fLIcu53PRuc6EIAM",
+  authDomain: "maxflash-663bc.firebaseapp.com",
+  projectId: "maxflash-663bc",
+  storageBucket: "maxflash-663bc.appspot.com",
+  messagingSenderId: "322027795655",
+  appId: "1:322027795655:web:2a215619bc7a87b618bae2",
+  measurementId: "G-94BDJKP6S6"
 };
 
 // Initialize Firebase
